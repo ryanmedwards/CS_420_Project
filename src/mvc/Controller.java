3ee6b5d93aa617b457ad22dd27e9227832b3641d
ryanmedwards <91635340+ryanmedwards@users.noreply.github.com>
2024-02-21 11:03:50 -0600

    Delete src directory

--- src/mvc/Controller.java
+++
@@ -1,6 +0,0 @@
-package mvc;
-
-public abstract class Controller<M extends Model<M, ?, ?, ?>> 
-{
-	public abstract void assign(final M model);
-}
--- src/opensky/OpenSky.java
+++
@@ -1,5 +0,0 @@
-package opensky;
-
-public class OpenSky {
-
-}
